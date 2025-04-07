7b71c66ad239de48b77d0a2446ea7ff6fd3e4d77
100RAV22 <saurav.kumar310704@gmail.com>
2025-04-07 14:12:54 +0530

    Added accordian project using vite

---
+++ Accordian/src/components/accordian/data.js
@@ -0,0 +1,23 @@
+const data = [
+	{
+	id : '1',
+	question : "What are accordian components?",
+	answer : "Accordian components are user interface elements used for show and hide content in a collapsible/expandable format"
+	},
+	{
+	id: '2',
+	question : "what are they used for?",
+	answer : "They are commonly employed in various contexts, including FAQS"
+	},
+	{
+	id: '3',
+	question : "Accordian as a musical instrument",
+	answer : "The accordian is a musical instrument with a keyboard" 
+	},
+	{
+	id: '4',
+	question : "Can I create an accordian component with a different from ",
+	answer : "Yes of course, it is very possible to create an accordion component"
+	}
+];
+export default data;
