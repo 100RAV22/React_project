eb848468b413a3fe4e648b487aaf717af2012366
100RAV22 <saurav.kumar310704@gmail.com>
2025-04-14 20:38:44 +0530

    Tree-View/ Menu-UI project

---
+++ Tree_Views/src/App.jsx
@@ -0,0 +1,13 @@
+import TreeView from './tree-view'
+import menus from './tree-view/data'
+
+function App() {
+  
+  return (
+    <>
+      <TreeView menus={menus}/>
+    </>
+  )
+}
+
+export default App
